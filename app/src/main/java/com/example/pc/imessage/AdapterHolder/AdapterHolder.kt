1aaa464bb371f4c56ffcca16ef63bbb68c5ffe67
Aldwin Tablante <aldwinotablante@gmail.com>
2018-05-27 00:39:02 +0800

    INIT

--- app/src/main/java/com/example/pc/imessage/AdapterHolder/AdapterHolder.kt
+++ app/src/main/java/com/example/pc/imessage/AdapterHolder/AdapterHolder.kt
@@ -28,7 +28,7 @@ class AdapterHolder(list: ArrayList<Contacts>, context: Context) : RecyclerView.
             intent.putExtra("email",data[position].Email)
             intent.putExtra("myemail",data[position].myemail)
             intent.putExtra("id",data[position].myid)
-            intent.putExtra("name",data[position].myname)
+            intent.putExtra("name",data[position].Username)
             v!!.context.startActivity(intent)
         }
 
